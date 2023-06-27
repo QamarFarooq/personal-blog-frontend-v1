dfc8f2fe263f5a29defaa812b228745a5ef7a286
QamarFarooq <ch.farooq.qamar@hotmail.com>
2023-06-27 16:09:19 -0400

    testing commit for the last time

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>I am simply testing for commit</div>
+        <div>This commit for last time</div>
         <div>testing git commmit again for second time</div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
