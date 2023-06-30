088ad2329355cd4bb7086fa95b4adf5f305594b7
QamarFarooq <ch.farooq.qamar@hotmail.com>
2023-06-29 22:08:27 -0400

    added basic font styling and also added react store and tested store access to make sure it works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 import {GlobalStore} from './store/Store';
 import React, { useState, useEffect, useReducer, createContext, useContext } from 'react';
+import axios from 'axios';
 
+// this format below is called REACT ARROW FUNCTION COMPONENT
 function App() {
 
   const [state, dispatch] = useContext(GlobalStore);
 
+  // when you console.log output from a axios request, do this "console.log("this is the response => ", response.data)"
+  // do NOT do this "console.log("this is the response => " + response.data)" if you do '+' the console log gives you [Object Object]
+
   console.log('I am accessting state.displayMatrix => ' + state.displayMatrix);
+
+  useEffect (() => {
+    axios.post('http://localhost:8081/').then(response => {
+      console.log("this is the response => ", response.data)
+    }).catch(error => {
+      console.log(error);
+    })
+
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>I am simply testing for commit</div>
-        <div>This commit for last time</div>
         <div>testing git commmit again for second time</div>
         <p>
           Edit <code>src/App.js</code> and save to reload!!!!!.
         </p>
-      </header>
+        <div className="nav-link">This is a link</div>
+        <div className="pagination-element">This is a pagination element</div>
+        <div className="button-element">This is a button element</div>
     </div>
   );
 }
